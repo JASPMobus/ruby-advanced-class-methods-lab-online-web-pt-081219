194147f727ca46c1458e6aa273cb19d0c6c55942
James Mobus <mobusj@live.com>
2019-09-10 19:34:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,8 +26,11 @@ class Song
   end
   
   def self.create_by_name(name)
-    song = self.new_by_name(name)
+    song = self.new
+    song.name = name
     
     @@all << song
+    
+    song
   end
 end
